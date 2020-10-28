e8d3a414dea5f1b576dea6f72c328e2ee466fa63
annieruth14 <ruthannie3@gmail.com>
2020-10-28 11:51:31 +0530

    [Annie]Added: test case for checking the top batting average cricketers

--- src/test/java/IplLeagueProblem/IPLLeagueAnalysisTest.java
+++ src/test/java/IplLeagueProblem/IPLLeagueAnalysisTest.java
@@ -2,10 +2,22 @@ package IplLeagueProblem;
 
 import org.junit.Test;
 
+import OpenCSV.CSVException;
+
+import org.junit.Assert;
+
 public class IPLLeagueAnalysisTest {
+	private static final String MOST_RUNS_FILE_PATH = "./src/test/resources/WP DP Data_01 IPL2019FactsheetMostRuns.csv";
 	IPLLeagueAnalysis analysis = new IPLLeagueAnalysis();
+
 	@Test
 	public void start() {
-		
+		analysis.welcomeMessage();
+	}
+	
+	@Test
+	public void givenAverage_whenHighest_shouldReturnHighest() throws IPLLeagueException {
+		RunsCSV player = analysis.getTopBattingAverages(MOST_RUNS_FILE_PATH);
+		Assert.assertEquals("MS Dhoni", player.player_name);
 	}
 }
