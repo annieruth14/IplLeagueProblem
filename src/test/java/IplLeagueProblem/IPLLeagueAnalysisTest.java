49c66af9a4cb0483bfd84a0dc6713fc1b78c9ffe
annieruth14 <ruthannie3@gmail.com>
2020-10-28 12:12:53 +0530

    [Annie]Added: test case to check the top striking rate of the batsman

--- src/test/java/IplLeagueProblem/IPLLeagueAnalysisTest.java
+++ src/test/java/IplLeagueProblem/IPLLeagueAnalysisTest.java
@@ -16,8 +16,14 @@ public class IPLLeagueAnalysisTest {
 	}
 	
 	@Test
-	public void givenAverage_whenHighest_shouldReturnHighest() throws IPLLeagueException {
+	public void givenAverage_whenHighest_shouldReturnPlayerHavingHighestAverage() throws IPLLeagueException {
 		RunsCSV player = analysis.getTopBattingAverages(MOST_RUNS_FILE_PATH);
 		Assert.assertEquals("MS Dhoni", player.player_name);
+	}
+	
+	@Test
+	public void givenStrikingRate_whenHighest_shouldReturnPlayerHavingHighestStrikingRate() throws IPLLeagueException {
+		RunsCSV player = analysis.getHighestStrikingRate(MOST_RUNS_FILE_PATH);
+		Assert.assertEquals("Ishant Sharma", player.player_name); 
 	}
 }
